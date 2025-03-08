90626a3212cdf9a1b8d3ac97589b97df360412b5
kaiobendrauf <38166195+kaiobendrauf@users.noreply.github.com>
2025-03-08 17:54:31 +0100

    notes and organisation

--- generate.py
+++ generate.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
+import logging
+import optparse
+import os
+import sys
+import urllib.error
+import urllib.request
+
+# ----------------------------------------
+#                mSCP
+# ----------------------------------------
+
+# ----------------------------------------
+#           Custom Baseline
+# ----------------------------------------
+
+
+# ----------------------------------------
+#              Munki Items
+# ----------------------------------------
+
+# ----------------------------------------
+#                 Main
+# ----------------------------------------
+
+def setup_logging():
+	logging.basicConfig(
+		level=logging.DEBUG,
+		format="%(asctime)s - %(levelname)s (%(module)s): %(message)s",
+		datefmt='%d/%m/%Y %H:%M:%S',
+		stream=sys.stdout)
 
 def main():
-	pass
+	setup_logging()
 
 if __name__ == '__main__':
 	main()
